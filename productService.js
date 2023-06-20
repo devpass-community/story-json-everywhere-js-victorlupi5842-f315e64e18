501ec43869b41be388ea46a66b17b042b207c0e7
Victor Lupi <victor.lupi@hotmail.com>
2023-06-20 02:32:53 +0000

    retorno resposta API

--- productService.js
+++ productService.js
@@ -2,6 +2,17 @@ async function getProduct(productId) {
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
     // Add your solution here!
+    try {
+        const response = await fetch(apiUrl); // Realiza a chamada para a API
+        const product = await response.json(); // Converte a resposta em JSON
+    
+        return product; // Retorna o objeto do produto
+      } catch (error) {
+        console.error('Erro ao obter o produto:', error);
+        throw error; // Lança o erro para ser tratado no código que chamou a função
+      }
+
+    
 }
 
 module.exports = getProduct;
