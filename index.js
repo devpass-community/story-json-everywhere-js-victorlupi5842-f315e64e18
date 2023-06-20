ded207d6b469cd40025316a9cf4c975ccb75c19b
Victor Lupi <victor.lupi@hotmail.com>
2023-06-20 03:00:39 +0000

    retorno resposta API

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const getProduct = require('./productService');
 (async() => {
     const productId = 10;
     const product = await getProduct(productId);
-    console.log(product.title);
+    console.log(product);
 })();
